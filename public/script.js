e6df662dce1ff85a4e88375064e8d5cf9d375643
IamLucidDreamer <shuklamanasofficial@gmail.com>
2021-08-20 06:53:47 -0700

    Test Commit

--- public/script.js
+++ public/script.js
@@ -1,3 +1,3 @@
-gsap.from( '#header', { duration: '2' , opacity: '0', ease: 'bounce' })
+gsap.from( '#header', { duration: '2' , opacity: '0', ease: 'circ' })
 
-gsap.from( '#text', { duration: '1.5' , y: '-100', ease: 'bounce' })
+gsap.from( '#text', { duration: '1.5' , y: '+100', opacity:'0', ease: 'circ' })
